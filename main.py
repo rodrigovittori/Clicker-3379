eac31976389f4840fa603380a8fdd7a7930a0b15
Rodrigo Vittori <vittorirodrigo@gmail.com>
2025-01-13 21:20:08 -0300

    M6.L3: Actividad #4 - "Temporizador"
    Implementamos lógica de bonificaciones idle y su compra

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 #pgzero
 """
-M6.L3: Actividad #3 - "Añadiendo bonificaciones"
-Objetivo: Agregar botones de bonus, (sólo dibujarlos)
+M6.L3: Actividad #4 - "Temporizador"
+Objetivo: Agregar lógica del click en los botones de bonificación pasiva y
+            las invocaciones/llamadas programadas (schedule_interval)
 
 PACK DE ASSETS: 
 ANIMALES: https://kenney.nl/assets/animal-pack-redux 
 BOTONES:  https://kenney.nl/assets/ui-pack
 
-Paso Nº 1: Crear Actores bonus_1 y bonus_2 así como sus valores : {precio (int), potenciador (int), ya_activado (bool)} 
-Paso Nº 2: Modificar nuestro draw() para que los muestre por pantalla, así como el texto que explica su función
+Paso Nº 1: Crear las funciones bonus
+Paso Nº 2: Comprobar click sobre botones
+Paso Nº 3: Verificar que el jugador tenga puntos suficientes para poder comprar el bonus (y restar esos puntos)
+Paso Nº 4: Programar la llamada reiterativa a las funciones bonus y setearlas como activas
 
-NOTA: La lógica de activar los botones la programaremos en el próximo ejercicio (no hay que agregar collidepoint todavía)
+Extra: agregar animaciones a los botones en caso de compra y en caso de NO poder comprarlo
+Extra Nº 2: Agregamos un "cheat" para poder probar los bonus sin farmear clicks
 
-Extra: Crear un token personalizado para nuestro juego
 
 """
 
@@ -48,6 +51,28 @@ bonus_3.potenciador = 50
 bonus_3.ya_activado = False
 """
 
+""" #####################
+   # FUNCIONES PROPIAS #
+  ##################### """
+
+def el_bonus_1():
+    global puntuacion
+    puntuacion += bonus_1.potenciador
+
+def el_bonus_2():
+    global puntuacion
+    puntuacion += bonus_2.potenciador
+
+"""
+def el_bonus_3():
+    global puntuacion
+    puntuacion += bonus_3.potenciador
+"""
+
+""" ####################
+   # FUNCIONES PGZERO #
+  #################### """
+
 def draw():
     fondo.draw()
     animal.draw()
@@ -75,9 +100,70 @@ def draw():
 def on_mouse_down(button, pos):
     global puntuacion
     if (button == mouse.LEFT):
+        
         if (animal.collidepoint(pos)):
             # Si el click fue en nuestro animal:
             puntuacion += click_mult
             # Animación:
             animal.y = 200
             animate(animal, tween="bounce_end", duration = 0.5, y = 250)
+
+        elif bonus_1.collidepoint(pos):
+            # Si el click fue sobre el botón de bonus # 1:
+            if (puntuacion >= bonus_1.precio):
+                # Chequeamos si tiene suficientes puntos para comprarlo:
+                # Chequeamos si ya está activo
+                if (not bonus_1.ya_activado):
+                    schedule_interval(el_bonus_2, 2)
+                    bonus_1.ya_activado = True
+                else:
+                    bonus_1.potenciador += 1
+                # Restamos los puntos gastados para compar el bonus:
+                puntuacion -= bonus_1.precio
+                # bonus_1.precio *= 2
+
+                # Animamos el botón cuando pueda comprarlo
+            
+            else:
+                # Si no tiene suficientes puntos para comprarlo
+                bonus_1.x = 445
+                animate(bonus_1, tween='bounce_end', duration=0.25, x=450)
+                bonus_1.x = 455
+                animate(bonus_1, tween='bounce_end', duration=0.25, x=450)
+
+        elif bonus_2.collidepoint(pos):
+            # Si el click fue sobre el botón de bonus # 2:
+            if (puntuacion >= bonus_2.precio):
+                # Chequeamos si tiene suficientes puntos para comprarlo:
+                # Chequeamos si ya está activo
+                if (not bonus_2.ya_activado):
+                    schedule_interval(el_bonus_2, 2)
+                    bonus_2.ya_activado = True
+                else:
+                    bonus_2.potenciador += 15
+                    
+                # Restamos los puntos gastados para compar el bonus:
+                puntuacion -= bonus_2.precio
+                # bonus_2.precio *= 2
+
+                # Animamos el botón cuando pueda comprarlo
+                
+            else:
+                # Si no tiene suficientes puntos para comprarlo
+                bonus_2.x = 445
+                animate(bonus_2, tween='bounce_end', duration=0.25, x=450)
+                bonus_2.x = 455
+                animate(bonus_2, tween='bounce_end', duration=0.25, x=450)
+
+        """ To-do: agregar lógica para el bonus 3 """
+
+# CHEAT:
+
+def on_key_down(key):
+    global puntuacion
+    
+    if keyboard.d:
+        puntuacion += 500
+        
+    if keyboard.a:
+        puntuacion -= 500
